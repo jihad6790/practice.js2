1e0d521d42867087e364719877b81968ce0ca0d0
jihad6790 <jihad123johad@gmail.com>
2023-09-23 10:55:46 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,31 @@
+// top 6
+
+// variable 
+let name="Sohag"
+let age=50
+let resuult = 39.49
+let isexist = true
+let ispassed = false
+let classes="11th"
+
+
+//  this is array 
+
+let namesArray = [1,2,3,4,5,6,7,8,9,10]
+
+function result(){
+   
+   let namesArray = ["jihad"]
+   console.log(namesArray )
+
+   let numbreArray = [2,3,4,6,]
+   console.log(numbreArray)
+
+   let sum=10+20
+   console.log(sum)
+
+   let result11 =  20*40
+   console.log(result11)
+}
+
+result()
